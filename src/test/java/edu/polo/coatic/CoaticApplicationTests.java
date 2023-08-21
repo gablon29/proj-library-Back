3d3fd18ae985308f2a2685b2e8fc9344a97360ea
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-21 19:19:24 -0300

    first commit

---
+++ src/test/java/edu/polo/coatic/CoaticApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.polo.coatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
