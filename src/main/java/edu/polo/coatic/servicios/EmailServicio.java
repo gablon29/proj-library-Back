bc2380c27872ec9223cee020117164d0976f9ade
gabri <gabriellondero40@gmail.com>
2023-09-14 07:53:04 -0300

    creando nuevo servicio de Eamil

---
+++ src/main/java/edu/polo/coatic/servicios/EmailServicio.java
@@ -0,0 +1,17 @@
+package edu.polo.coatic.servicios;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.mail.*;
+import org.springframework.mail.javamail.*;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+public class EmailServicio {
+    
+}
